5f4ac74998fa25cb81b99c79b003353b36a3068d
leshchev alekcei <luolikl@rambler.ru>
2022-11-26 13:21:18 +0300

    Tasc2

--- tasc1/Program.cs
+++ tasc1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Tasc2
+Console.WriteLine("Введите первое число ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число ");
+int m = Convert.ToInt32(Console.ReadLine());
+int max = n;
+if (m > max)
+    Console.Write($"max = {m}");
+else 
+    Console.Write($"max = {n}");
