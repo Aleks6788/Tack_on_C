fd253d0c406760cecb00b2aae4390ee454b59d6e
leshchev alekcei <luolikl@rambler.ru>
2022-11-29 13:03:20 +0300

    tasc13

---
+++ tasc13_n/Program.cs
@@ -0,0 +1,11 @@
+﻿// Tasc13
+Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 100)
+    Console.Write("No");
+else 
+{
+    while (n > 1000)
+        n = (n / 10);
+        Console.Write(n % 10);
+}
